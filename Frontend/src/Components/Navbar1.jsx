f85663c3092b779d1baf239ace3309efc5405c6b
Kshitij Kotecha <kotechakshitij@gmail.com>
2024-08-03 10:41:56 +0530

    Landing Page

---
+++ Frontend/src/Components/Navbar1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar1.css";
+
+const Navbar1 = () => {
+  return (
+    <div>
+      <div className="MainBoxN1">
+        <div className="LogoN1">
+          <h1>TypeRacer</h1>
+        </div>
+        <div className="RegisterN1">
+          <div className="RegisterUserN1"><h3>SignUp/SignIn</h3></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar1;
